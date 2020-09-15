9853ad068936d4a1eef8521a6d2a1b582f049fc2
Samuel <maxwell.moyya@gmail.com>
2020-09-14 22:42:42 -0500

    Added showcase scene + quit, restart buttons.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKeyDown("backspace"))
+        {
+            SceneManager.LoadScene("Proj_01_Full");
+        }
+    }
+}
