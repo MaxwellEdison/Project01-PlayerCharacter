55d9d552cf5f8d81ab00cc981315e2f93142f016
Samuel <maxwell.moyya@gmail.com>
2020-10-11 04:05:40 -0500

    Added (nonfunctional) basic UI Added basic hazard

---
+++ Assets/Scripts/EnvironmentDamage.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class EnvironmentDamage : MonoBehaviour
+{
+    public enum EnvironmentDamageType
+    {
+        spikes,
+        lava,
+        pit
+    }
+    public EnvironmentDamageType dmgObject;
+    public int spikeDmg = 10;
+    public int lavaDmg = 20;
+    public int pitDmg = 100;
+    private int dmgValue;
+    
+    private void SetDmg()
+    {
+        switch (dmgObject)
+        {
+            case EnvironmentDamageType.spikes:
+                dmgValue = spikeDmg;
+                break;
+            case EnvironmentDamageType.lava:
+                dmgValue = lavaDmg;
+                break;
+            case EnvironmentDamageType.pit:
+                dmgValue = pitDmg;
+                break;
+        }
+        Debug.Log("I am hazard type " + dmgObject + " with a damage value of " + dmgValue);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        SetDmg();
+        if(other.gameObject.tag == "Player")
+        {
+
+            GameObject player = other.gameObject;
+            Debug.Log("I damaged the player for " + dmgValue + " and they currently have " + player.GetComponent<Health>()._currentHealth + " hp");
+            player.GetComponent<Health>()._currentHealth -= dmgValue;
+            if(player.GetComponent<Health>()._currentHealth <= 0)
+            {
+                player.GetComponent<Health>().Kill();
+            }
+        }
+    }
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,10 +3,47 @@
 public class Health : MonoBehaviour
 {
     public int _currentHealth = 50;
+    public enum HealthType
+    {
+        player,
+        enemy01,
+        enemy02
+    }
+    public HealthType healthType;
+    public int player = 100;
+    public int enemy01 = 10;
+    public int enemy02 = 20;
+    private int maxHealth;
+
+    private void Start()
+    {
+        switch (healthType)
+        {
+            case HealthType.player:
+                maxHealth = player;
+                break;
+            case HealthType.enemy01:
+                maxHealth = enemy01;
+                break;
+            case HealthType.enemy02:
+                maxHealth = enemy02;
+                break;
+        }
+        Debug.Log("I am health type " + healthType + " with a max health of " + maxHealth);
+    }
 
     public void Heal(int amount)
     {
+
         _currentHealth += amount;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, maxHealth);
         Debug.Log(gameObject.name + " has healed " + amount);
+    }
+    public void Kill()
+    {
+        if(_currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
